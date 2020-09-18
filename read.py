46d49f161966648b485eeee8f0b32dd8d0c792ca
Raymond Liu <wfliu66@qq.com>
2020-09-18 14:03:05 +0100

    calc average reviews

--- read.py
+++ read.py
@@ -6,8 +6,10 @@ with open('reviews.txt', 'r') as f:
         count += 1
         if count % 1000 == 0:
             print(len(data))
-print(len(data))
-#print(data)
-print(data[0])
-print('..........................')
-print(data[1])
+print('file is read, total', len(data), 'data')
+
+sum_len = 0
+for d in data:
+    sum_len = sum_len + len(d)
+print(sum_len)
+print('Average line length is', sum_len/len(data))
